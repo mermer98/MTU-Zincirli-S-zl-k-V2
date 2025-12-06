2b91b5379a3af496ed6ac38a8aea0c4db8108eb1
mermer98 <mermer98@gmail.com>
2025-12-06 17:58:20 +0300

    Railway için PORT environment variable desteği eklendi

--- web_app.py
+++ web_app.py
@@ -481,4 +481,7 @@ if __name__ == '__main__':
     print("Tarayıcıda açın: http://localhost:5000")
     print("=" * 50)
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # Railway için port ayarı
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
